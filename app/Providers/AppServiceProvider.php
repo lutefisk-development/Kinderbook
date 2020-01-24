b449ca38aa5f23cbe4868617fa0c2cfb77e8d06f
pks-development <kristiansvanberg@gmail.com>
2020-01-24 10:47:10 +0100

    Change default string length
    Changed the default string length, to avoid getting sql error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /*
+            since laravel uses the "utf8mb4" character set by default, (specified in /config/database.php) which is 4 bytes and the max key length is 767
+            we need to change the string length to 191.
+
+            before change: 4 * 255 = 1020 which is more then 767, will result in sql error.
+            after change: 4 * 191 = 764 which is less then 767
+        */
+        Schema::defaultStringLength(191);
     }
 }
