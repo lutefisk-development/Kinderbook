6422d94d2546847ec7ea1228800ad2f1171f00f1
pks-development <kristiansvanberg@gmail.com>
2020-01-29 15:07:19 +0100

    updated User model
    Updated fillable array for accepting mass assignment from controller

--- app/User.php
+++ app/User.php
@@ -16,7 +16,13 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 
+        'email', 
+        'password',
+        'address',
+        'city',
+        'phone_nr',
+        'zip',
     ];
 
     /**
