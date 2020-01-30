e8c421d6a18629744e373b97bf573d4917d66f80
pks-development <kristiansvanberg@gmail.com>
2020-01-30 16:06:10 +0100

    Created Controller
    Created Kid resource controller

---
+++ app/Http/Controllers/KidController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Kid;
+use App\Http\Resources\Kid as KidResource;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+
+
+class KidController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Kid  $kid
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Kid $kid)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Kid  $kid
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Kid $kid)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Kid  $kid
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Kid $kid)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Kid  $kid
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Kid $kid)
+    {
+        //
+    }
+}
