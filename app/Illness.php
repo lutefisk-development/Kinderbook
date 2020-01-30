85b3faa0fddc94e58fc90920582eee61aaf3f690
pks-development <kristiansvanberg@gmail.com>
2020-01-30 12:26:01 +0100

    Ilness Model
    Created Illness model and added relations to other models.

---
+++ app/Illness.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Kid;
+
+class Illness extends Model
+{
+    //protected $fillable[];
+
+    /**
+     * Relations to other models
+     */
+    public function kids()
+    {
+        $this->belongsToMany(Kid::class);
+    }
+}
