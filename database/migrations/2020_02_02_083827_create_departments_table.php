0a2a8dbfeb3bf477711be50c57e1029656898373
pks-development <kristiansvanberg@gmail.com>
2020-02-02 10:07:58 +0100

    Created migration
    Created migration for departments table

---
+++ database/migrations/2020_02_02_083827_create_departments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDepartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('kindergarten_id')->index();
+            $table->foreign('kindergarten_id')->references('id')
+                ->on('kindergartens');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departments');
+    }
+}
