377faa3f84fbd8b065b870edec6e4b9ea7eb74b8
pks-development <kristiansvanberg@gmail.com>
2020-02-02 09:45:55 +0100

    Created migration
    Created migration for roles table

---
+++ database/migrations/2020_02_02_083732_create_roles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->foreign('user_id')->references('id')
+                ->on('users');
+            $table->string('role_name');
+            $table->boolean('is_admin')-deafult(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
