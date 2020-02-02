5d13ac31e0eef701dd35b1d91403872056f00b6f
pks-development <kristiansvanberg@gmail.com>
2020-02-02 10:11:45 +0100

    Created migartion
    Created migration for announcements table

---
+++ database/migrations/2020_02_02_083853_create_announcements_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnnouncementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('announcements', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('department_id')->index();
+            $table->foreign('department_id')->references('id')
+                ->on('departments');
+            $table->string('title');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('announcements');
+    }
+}
