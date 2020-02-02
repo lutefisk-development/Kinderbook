3444f9b2cc05d61292445a92862f79f1f9383526
pks-development <kristiansvanberg@gmail.com>
2020-02-02 11:40:46 +0100

    Changed relations
    Changed how Role and User models relates to eachother.

--- app/Role.php
+++ app/Role.php
@@ -14,6 +14,6 @@ class Role extends Model
      */
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->hasOne(User::class);
     }
 }
--- app/User.php
+++ app/User.php
@@ -55,6 +55,6 @@ class User extends Authenticatable
 
     public function role()
     {
-        return $this->hasOne(Role::class);
+        return $this->belongsTo(Role::class);
     }
 }
