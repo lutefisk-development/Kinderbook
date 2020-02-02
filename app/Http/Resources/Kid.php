5b6e28ec8f41f1d852ae739d5836e724ff0569cb
pks-development <kristiansvanberg@gmail.com>
2020-02-02 13:51:33 +0100

    Update resource
    Updated Kid resource, for displaying correct information via the api.

--- app/Http/Resources/Kid.php
+++ app/Http/Resources/Kid.php
@@ -17,7 +17,10 @@ class Kid extends JsonResource
         // return parent::toArray($request);
         return [
             'id' => $this->id,
-            'name' => $this->name,
+            'department_id' => $this->department_id,
+            'parent' => $this->user->first_name . ' ' . $this->user->last_name,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
             'image_url' => $this->image['path'],
             'is_present' => $this->is_present,
             'messages' => $this->messages,
