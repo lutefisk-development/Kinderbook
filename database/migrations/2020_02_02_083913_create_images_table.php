be3f536caeb83ff25c7d438668cc4de53719c834
pks-development <kristiansvanberg@gmail.com>
2020-02-02 10:19:44 +0100

    Created migration
    Created migration for images table

---
+++ database/migrations/2020_02_02_083913_create_images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('announcement_id')->nullable();
+            $table->unsignedInteger('kid_id')->nullable();
+            $table->string('path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
