ea2425e9ad626929f1afa253c4d8d5ee99af587b
pks-development <kristiansvanberg@gmail.com>
2020-02-02 09:50:34 +0100

    Created migration
    Created migration for kindergartens table

---
+++ database/migrations/2020_02_02_083759_create_kindergartens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKindergartensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kindergartens', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('address');
+            $table->string('city');
+            $table->string('email');
+            $table->string('name');
+            $table->string('phone_nr');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kindergartens');
+    }
+}
