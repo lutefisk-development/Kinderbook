e94782986a9cb9f641d1e0584cb79538dc1dccc1
pks-development <kristiansvanberg@gmail.com>
2020-02-08 17:28:15 +0100

    home and welcome views
    Updated the home and welcome view, to be more welcomming for users. also adding SASS and compiled CSS for the views in this commit.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,42 @@
-@extends('layouts.app')
+@extends('layouts.login')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+@include('partials/status')
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="text-center first">Välkommen till</h1>
+            <h1 class="text-center second">STORKBOET</h1>
+        </div>
+    </div>
+    <div class="container layouts-wrapper">
+        <div class="row">
+            <div class="col-md-6 text-inside-layouts-wrapper">
+                <div>
+                    <p>
+                        Det ska vara roligt och spännande att vara barn på I Ur och Skur Storkboet. Här utgår vi ifrån barnens erfarenheter, intressen, behov och åsikter. Vi upptäcker och upplever med alla våra sinnen i alla aktiviteter, både ute och inne. Här ser vi inte bara gruppen utan även individen i gruppen, det är viktigt att alla känner sig trygga och att barnen får vara barn.
+                    </p>
+                    <p>
+                        "Det är ute i verkligheten, i utemiljön, i landskapet som människor lär bäst. Det är där man upplever med alla sinnen och både ser, hör, känner, smakar och förnimmer. Det främjar både nyfikenhet, kreativitet och samarbete, väcker känslor och får oss att bry oss om vår miljö, natur - och kulturlandskapet och vår egen historia." Anders Szczepanski LiU.
+                    </p>
                 </div>
+            </div>
+            <div class="col-md-6">
+                <img src="{{ Storage::url('/pictures/stork.jpg') }}" alt="" class="img-fluid">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <img src="{{ Storage::url('/pictures/kids.jpeg') }}" alt="" class="img-fluid">
+            </div>
+            <div class="col-md-6 text-inside-layouts-wrapper">
+                <p>
+                    I Ur och Skur Storkboet är ett personalkooperativ som drivs som ekonomisk förening, av Malin Fredriksson och Lotta Forsberg.
+                    Förskolans vision är ”Barns lärande genom upplevelser”
+                    Förskolan får bidrag från Sjöbo kommun till förskoleverksamheten, och förskolan följer reglerna utifrån maxtaxan och allmän förskola. Vi driver också verksamheten utifrån Friluftsfrämjandets krav att få kalla sig I Ur och Skur förskola. Idén i denna pedagogik är att barns behov av kunskap, rörelse och gemenskap tillfredsställs genom vistelse i naturen.
+                    Våra öppettider är 6.00-17.15, övriga krav eller regler står i vårt kontrakt som föräldrarna skriver på vid inskolningen.
+                </p>
             </div>
         </div>
     </div>
-</div>
 @endsection
+
