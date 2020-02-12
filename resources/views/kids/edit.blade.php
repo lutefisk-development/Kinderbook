c799f47661d232618360e3d4d8f6c69cf5b34d06
pks-development <kristiansvanberg@gmail.com>
2020-02-12 17:44:21 +0100

    Update edit.blade.php
    Finished working on view where a user can update information about their kids, also admins can change what department a kid belongs to.

--- resources/views/kids/edit.blade.php
+++ resources/views/kids/edit.blade.php
@@ -4,10 +4,10 @@
     <div class="container" id="edit-child-wrapper">
         <h1 class="text-center mb-3 mt-3">Här ändrar du uppgifter för {{ $kid->first_name }}:</h1>
 
-        <form 
-        method="POST" 
-        action="{{ route('kids.update', ['kid' => $kid->id]) }}" 
-        id="edit-child" 
+        <form
+        method="POST"
+        action="{{ route('kids.update', ['kid' => $kid->id]) }}"
+        id="edit-child"
         enctype="multipart/form-data">
             @csrf
             @method('PUT')
@@ -24,22 +24,20 @@
 
                 <div class="form-row">
                     <div class="form-group col-md-6">
-                        <input 
-                        id="first_name" 
-                        type="hidden" 
-                        class="form-control" 
-                        name="first_name" 
-                        required 
+                        <input
+                        id="first_name"
+                        type="hidden"
+                        class="form-control"
+                        name="first_name"
                         value="{{  $kid->first_name }}">
                     </div>
 
                     <div class="form-group col-md-6">
-                        <input 
+                        <input
                         id="last_name"
                         type="hidden"
                         class="form-control"
                         name="last_name"
-                        required
                         value="{{$kid->last_name}}">
                     </div>
             @endif
@@ -48,39 +46,48 @@
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="first_name">Barnets förnamn:</label>
-                        <input 
-                        id="first_name" 
-                        type="text" 
-                        class="form-control" 
-                        name="first_name" 
-                        required 
-                        value="{{ old('first_name') ? old('first_name') : $kid->first_name}}"
-                        placeholder="{{ $kid->first_name }}">
+                        <input
+                        id="first_name"
+                        type="text"
+                        class="form-control"
+                        name="first_name"
+                        value="{{ old('first_name') ? old('first_name') : $kid->first_name}}">
                     </div>
 
                     <div class="form-group col-md-6">
                         <label for="last_name">Barnets efternamn:</label>
-                        <input 
+                        <input
                         id="last_name"
                         type="text"
                         class="form-control"
                         name="last_name"
-                        required
-                        value="{{ old('last_name') ? old('last_name') : $kid->last_name}}"
-                        placeholder="{{ $kid->last_name }}">
+                        value="{{ old('last_name') ? old('last_name') : $kid->last_name}}">
                     </div>
                 </div>
-                
+
                 @if(!isset($kid->image->path))
                     <div class="form-group">
                         <label for="path">Lägg upp ett kort på ditt barn:</label>
-                        <input 
-                        id="path" 
-                        type="file" 
-                        class="form-control-file" 
+                        <input
+                        id="path"
+                        type="file"
+                        class="form-control-file"
                         name="file">
                     </div>
                 @endif
+
+                <hr>
+
+                <p>Sjukanmäla ditt barn:</p>
+
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <input type="date" class="form-control mt-2" name="date_start" value="{{ old('date_start') }}">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="date" class="form-control mt-2" name="date_end" value="{{ old('date_end') }}" min="{{ $tomorrow }}">
+                    </div>
+                </div>
             @endif
 
             <!-- Displaying errors if user tries to enter false information -->
