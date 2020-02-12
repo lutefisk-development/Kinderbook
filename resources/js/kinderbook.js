0a122a1fcf19d4edac27d7eff91f8b37299e98af
pks-development <kristiansvanberg@gmail.com>
2020-02-12 17:49:43 +0100

    Update kinderbook.js
    Updated JavaScript, added logic for handling a click event. When admins click on a picture, the color changes. To keep track on who is currently present.

--- resources/js/kinderbook.js
+++ resources/js/kinderbook.js
@@ -10,3 +10,19 @@
     }, 3000);
 
 })(jQuery);
+
+// Change the color on kids card, based on when they arrive to preschool. If the user if an admin!
+if(user[0] === 1 || user[0] === 2) {
+    document.body.addEventListener('click', e => {
+
+        const target = e.target.parentElement.classList;
+
+        if(target.contains('blue')) {
+            target.add('green');
+            target.remove('blue');
+        } else if (target.contains('green')) {
+            target.add('blue');
+            target.remove('green');
+        }
+    });
+}
