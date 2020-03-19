cd130ce94dd369ef2c207f13c47680c15e7679d9
pks-development <kristiansvanberg@gmail.com>
2020-03-19 15:28:11 +0100

    Added conditional
    Added conditional for checking if a user has registrated a child to the daycare. and displays content based on true/false

--- resources/views/own-kids.blade.php
+++ resources/views/own-kids.blade.php
@@ -3,21 +3,25 @@
 @section('content')
     @include('partials/status')
     <div class="container" id="own-kids-wrapper">
-        <h1 class="text-center mb-3 mt-3">Dina barn i Storkboet:</h1>
-        <div class="row kids">
-            @foreach($kids as $kid)
-                <div class="col-md-4 col-lg-3 d-flex align-items-stretch">
-                    <div class="card card-body mt-2 kid">   
-                        @if(isset($kid->image->path))
-                            <img src="{{ $kid->image->path }}" alt="" class="card-img-top img-fluid" id="kid-image">
-                        @else
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" alt="" class="card-img-top img-fluid" id="kid-image">
-                        @endif
-                        <p class="card-text text-center mt-2">{{ $kid->first_name }}&nbsp;{{ $kid->last_name }}</p>
-                    <a href="{{ route('kids.show', ['kid' => $kid->id]) }}" class="btn btn-light btn-block">Ändra {{ $kid->first_name }}s uppgifter</a>
+        @if(count($kids) == 0)
+            <h1 class="text-center mb-3 mt-3">Du har inga barn registrerat på Storkboet</h1>
+        @else
+            <h1 class="text-center mb-3 mt-3">Dina barn i Storkboet:</h1>
+            <div class="row kids">
+                @foreach($kids as $kid)
+                    <div class="col-md-4 col-lg-3 d-flex align-items-stretch">
+                        <div class="card card-body mt-2 kid">
+                            @if(isset($kid->image->path))
+                                <img src="{{ $kid->image->path }}" alt="" class="card-img-top img-fluid" id="kid-image">
+                            @else
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" alt="" class="card-img-top img-fluid" id="kid-image">
+                            @endif
+                            <p class="card-text text-center mt-2">{{ $kid->first_name }}&nbsp;{{ $kid->last_name }}</p>
+                        <a href="{{ route('kids.show', ['kid' => $kid->id]) }}" class="btn btn-light btn-block">Ändra {{ $kid->first_name }}s uppgifter</a>
+                        </div>
                     </div>
-                </div>
-            @endforeach
-        </div>
+                @endforeach
+            </div>
+        @endif
     </div>
 @endsection
